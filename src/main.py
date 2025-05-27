88b3bbca7ccbc3e45e979a49811d14c20ba70e07
Max <massimo.odisio@gmail.com>
2025-05-27 22:45:54 +0200

    move text to html function to an independent file

--- src/main.py
+++ src/main.py
@@ -1,21 +1,4 @@
-from htmlnode import HTMLNode, LeafNode, ParentNode
 from textnode import TextNode, TextType
-
-
-def text_node_to_html_node(text_node):
-    match text_node:
-        case TextType.TEXT:
-            return LeafNode(text_node)
-        case TextType.BOLD:
-            return LeafNode("b", text_node)
-        case TextType.ITALIC:
-            return LeafNode("i", text_node)
-        case TextType.CODE:
-            return LeafNode("code", text_node)
-        case TextType.LINK:
-            return LeafNode("a", text_node, "href")
-        case TextType.IMAGE:
-            return LeafNode("img", None, ["src", "alt"])
 
 
 def main():
